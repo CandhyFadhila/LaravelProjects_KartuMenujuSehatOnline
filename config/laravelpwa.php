f2205d32ae31c171ba9a9bf1e02a13a6b7572fd2
CandhyFadhila <omaplocandhy@gmail.com>
2023-05-25 06:54:20 +0700

    Set up Name Installed PWA

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -4,7 +4,7 @@ return [
     'name' => 'LaravelPWA',
     'manifest' => [
         'name' => env('APP_NAME', 'My PWA App'),
-        'short_name' => 'PWA',
+        'short_name' => 'KMS Online',
         'start_url' => '/',
         'background_color' => '#ffffff',
         'theme_color' => '#000000',
