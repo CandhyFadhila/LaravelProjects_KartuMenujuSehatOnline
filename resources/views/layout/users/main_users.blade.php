a594b028b35b77f709f00188802774c1af5565bc
CandhyFadhila <omaplocandhy@gmail.com>
2023-09-16 18:05:59 +0700

    Remove old version PWAs

--- resources/views/layout/users/main_users.blade.php
+++ resources/views/layout/users/main_users.blade.php
@@ -39,16 +39,12 @@
 
      {{-- ! AJAX --}}
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-
-     {{-- ! LARAVEL PWA SECTION --}}
-     @laravelPWA
-
      <!-- =======================================================
   * Template Name: MyResume - v4.10.0
   * Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
-  ======================================================== -->
+======================================================== -->
      {{-- ! CSS FOR TABEL IMUNISASI --}}
      <style>
           /* ! Add css div_table_vitamin */
